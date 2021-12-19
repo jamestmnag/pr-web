40874b7121d2ea217c1c14e608bdd9992dc04d6b
james tamang <jamstmnag@gmail.com>
2021-12-19 06:03:35 +0545

    zip file

--- projectlaravel/routes/web.php
+++ projectlaravel/routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
+|~
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
@@ -16,6 +17,5 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/',[PagesController::class, 'index']);
+Route::get('/about', [PagesController::class,'about']);
